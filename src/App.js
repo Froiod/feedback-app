6d57271c6550215fc339fb1419316a01de76d2ff
Paolo Guray <froiodpaolo@gmail.com>
2023-01-18 21:12:36 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import Header from "./components/Header"
 
 
 function App () {
 
   return (
-    
-      
+    <>
+      <Header />
       <div className="container">
+
       </div>
+    </>    
   )
 }
 
