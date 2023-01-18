472964cb1765aad109ed1a2b01a0a5b61f73e700
Paolo Guray <froiodpaolo@gmail.com>
2023-01-18 20:46:17 +0800

    starting files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 
-import {useState} from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
 
 function App () {
-  const [feedback, setfeedback] = useState(FeedbackData)
 
   return (
-    <>
-      <Header />
+    
+      
       <div className="container">
-        <FeedbackList feedback={feedback} />
       </div>
-
-    </>
   )
 }
 
