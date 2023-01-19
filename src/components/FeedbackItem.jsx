d32fffffb83c393ded47a7aa609bb718045d255c
Paolo Guray <froiodpaolo@gmail.com>
2023-01-19 21:48:02 +0800

    added x button

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -7,6 +7,9 @@ function FeedbackItem({item}) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className='close'>
+        <FaTimes color='purple'/>
+      </button>
       <div className="text-display">
         {item.text}
       </div>
